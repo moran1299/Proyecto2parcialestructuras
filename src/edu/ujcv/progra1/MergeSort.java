ce41bed5f3e5caf633cd34ee90b5d55abdc84cfb
Laptop <iliana.moran@ujcv.edu.hn>
2020-03-18 14:38:52 +0100

    MergeSort

--- src/edu/ujcv/progra1/MergeSort.java
+++ src/edu/ujcv/progra1/MergeSort.java
@@ -1,23 +1,71 @@
 package edu.ujcv.progra1;
 
-public class MergeSort  implements SortTester{
-    @Override
-    public long sort(int[] array) {
-        long start = System.currentTimeMillis();
-        mergeSort(array);
+public class MergeSort {
+    void merge(int arr[], int beg, int mid, int end){
 
-        long end = System.currentTimeMillis();
+        int l = mid - beg + 1;
+        int r = end - mid;
 
-        return end - start;
+        int LeftArray[] = new int[l];
+        int RightArray[] = new int[r];
+
+        for (int i=0; i<l; ++i)
+            LeftArray[i] = arr[beg + i];
+
+        for (int j=0; j<r; ++j)
+            RightArray[j] = arr[mid + 1+ j];
+
+        int i = 0, j = 0;
+        int k = beg;
+        while (i<l&&j<r)
+        {
+            if (LeftArray[i] <= RightArray[j])
+            {
+                arr[k] = LeftArray[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = RightArray[j];
+                j++;
+            }
+            k++;
+        }
+        while (i<l)
+        {
+            arr[k] = LeftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j<r)
+        {
+            arr[k] = RightArray[j];
+            j++;
+            k++;
+        }
     }
 
-    public static int[] mergeSort(int[] a){
-        return new int[5];
+    void sort(int arr[], int beg, int end)
+    {
+        if (beg<end)
+        {
+            int mid = (beg+end)/2;
+            sort(arr, beg, mid);
+            sort(arr , mid+1, end);
+            merge(arr, beg, mid, end);
+        }
     }
+    public static void main(String args[])
+    {
+        int arr[] = {90,23,101,45,65,23,67,89,34,23};
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length-1);
 
-    public static int[] merge(int[] inf, int[] sup ){
-        return new int[5];
+        System.out.println("\nSorted array");
+        for(int i =0; i<arr.length;i++)
+        {
+            System.out.println(arr[i]+"");
+        }
     }
-
-
 }
